d34c4bddcf9196ba72902f149d405f671485970f
Nicholas Smith <nsmith@edgeworthsecurity.com>
2023-08-08 16:41:47 -0400

    first commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <!-- Add your footer content here -->
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<header>
+
+    <!-- Add your logo image here -->
+    <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="Site Logo">
+</header>
+
+
+<?php get_footer(); ?>
